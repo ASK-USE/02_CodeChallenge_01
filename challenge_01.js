85cb244ba50729c221094c5903dd550fd1763194
ASK-DONE <knueppelalex@web.de>
2023-03-30 15:35:35 +0200

    challenge cleared

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,10 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let firstNumber, secondNumber, summe;
+
+firstNumber = prompt("Bitte erste Zahl eingeben:");
+secondNumber = prompt("Bitte zweite Zahl eingeben:"); 
+summe = parseInt(firstNumber) + parseInt(secondNumber);
+
+console.log(firstNumber + " + " + secondNumber + " = " + summe);
